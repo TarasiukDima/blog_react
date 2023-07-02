7273e96f0caf36d029112b1bb0e9f3afb9b7c9d4
Dzmitry Tarasiuk <dimatarasiuk91@gmail.com>
2023-07-02 20:22:09 +0300

    feat: add concurency for start one command, refactor some code

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -11,7 +11,18 @@ export function useTheme(): IUseThemeResult {
   const { theme, setTheme } = useContext(ThemeContext);
 
   const toggleTheme = () => {
-    const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+    let newTheme: Theme;
+    switch (theme) {
+    case Theme.DARK:
+      newTheme = Theme.LIGHT;
+      break;
+    case Theme.LIGHT:
+      newTheme = Theme.DARK;
+      break;
+    default:
+      newTheme = Theme.DARK;
+    }
+
     setTheme?.(newTheme);
 
     localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -3,8 +3,8 @@ import { RouteObject } from "react-router-dom";
 export type TRouteObject = RouteObject & { routeTextKey: string };
 
 export enum Theme {
-  LIGHT = "light",
-  DARK = "dark",
+  LIGHT = "light_theme",
+  DARK = "dark_theme",
 }
 
 export enum ButtonSize {
